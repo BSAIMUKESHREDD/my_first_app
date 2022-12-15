66d782c810435d2b847cfac552629825c66abc22
B SAI MUKESH REDDY <60388830+BSAIMUKESHREDD@users.noreply.github.com>
2022-12-15 12:12:17 +0530

    addingImageFromInternet

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-            color: Colors.black,
-           // shape:BoxShape.circle,
+        body: const Center(
+          
+          child: Image(image: NetworkImage('https://images.unsplash.com/photo-1670968982579-a0161d82234c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'),
 
           ),
+          
         ),
        // backgroundColor: Colors.blueAccent,
         appBar: AppBar(backgroundColor: Colors.black,
