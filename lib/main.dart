2563661550e1269785f246aae0adfb1e98e9e1f5
B SAI MUKESH REDDY <60388830+BSAIMUKESHREDD@users.noreply.github.com>
2022-12-15 12:58:26 +0530

    addingImageFromPersonalPC

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,11 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: const Center(
           
-          child: Image(image: NetworkImage('https://images.unsplash.com/photo-1670968982579-a0161d82234c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'),
-
+          //child: Image(image: AssetImage(assetName)),
+//inpubspec.yaml file under assets add the folder just created and copy the url in assetimage folder
           ),
           
-        ),
+
        // backgroundColor: Colors.blueAccent,
         appBar: AppBar(backgroundColor: Colors.black,
           title: Text("hi im me"),
